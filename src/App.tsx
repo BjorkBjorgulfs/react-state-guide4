5b73d0a5d138d290de701f7abb4b615f7144c45f
BjorkBjorgulfs <bogga1387@gmail.com>
2023-11-04 12:36:11 +0000

    made card and added a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from './Card';
+import Button from './Button';
 
 function App() {
+  const buttons = [
+    {title: "UK Royal family"},
+    {title: "DK Royal family"},
+    {title: "Hollywood Royal family"}
+  ]
+  const profiles = [
+    {
+      pictureURL:"https://th.bing.com/th/id/OIP.0bJlSBwgMIj9_sWXanRgaAHaGj?pid=ImgDet&rs=1",
+      name: "King Charles",
+      info: "King Charles is the head of the family after his mother, Queen Elizabeth, died in 2023"
+    },
+    {
+      pictureURL: "https://www.thefamouspeople.com/profiles/images/prince-william-4.jpg",
+      name: "Prince William",
+      info: "Prince William is the oldest son of King Charles, he is the next heir of the crown"
+    },
+    {
+      pictureURL: "https://th.bing.com/th/id/OIP.xiCHWq3XZ2UvgKlSQwDxLgHaFj?pid=ImgDet&rs=1",
+      name: "Princess Anne",
+      info: "Princess Anne is the younger sister of King Charles, she's a badass"
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        {buttons.map((button)=>{
+          return <Button title={button.title}></Button>
+        })}
+      </nav>
+      <div><h1>Here is a card and button I made using react-props</h1>
+      </div>
+      <div>
+        {profiles.map((profile)=>{
+            return <Card pic={profile.pictureURL} name={profile.name} info={profile.info}></Card>
+        })}
+      </div>
+      
     </div>
   );
 }
---
+++ src/Button.tsx
@@ -0,0 +1,28 @@
+import './button.css';
+
+type Props = {
+    color?:string,
+    title:string
+}
+const Button = ({color, title}:Props)=> {
+    const styles = {
+        backgroundColor : color || "#C49D9D",
+        borderRadius: "60px",
+        height: "40px",
+        width: "auto",
+        color: "#F5F5F5",
+        border: "2px solid #C49D9D",
+        paddingLeft: "10px",
+        paddingRight: "10px",
+    }
+    //then I made a hover effect using CSS
+    
+    return (
+        <>
+            <button className="button" style = {styles}>{title}</button>
+        </>
+    )
+
+}
+
+export default Button;
